771db0335a6daee1d6ef479b219244c99e3ef9d6
Sergey <splotnikov@gmail.com>
2021-01-22 17:19:15 -0500

    Added solution

--- AzureFuncs/HttpTriggerFunc.cs
+++ AzureFuncs/HttpTriggerFunc.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,35 +8,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Corlate.Function
+namespace AzureFuncs
 {
-    public static class HttpTrigger
+    public static class HttpTriggerFunc
     {
-        [FunctionName("Scream")]
+        [FunctionName("HttpTriggerFunc")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "scream/hello")] HttpRequest req,
-            [Queue("outqueue"), StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
-            log.LogInformation($"name: {name}");
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            if (!string.IsNullOrEmpty(name))
-            {
-                // Add a message to the output collection.
-                msg.Add(string.Format("Name passed to the function: {0}", name));
-            }
-
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Passed name: {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
